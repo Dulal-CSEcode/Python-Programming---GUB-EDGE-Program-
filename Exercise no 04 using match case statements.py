7c2c083cebdbd00578a674f0ee12edcd41d9decc
MD Dulal Hossain <dulal.md.cse@gmail.com>
2025-05-31 02:35:03 +0600

    Delete Exercise no 04 using match case statements.py

--- Exercise no 04 using match case statements.py
+++
@@ -1,9 +0,0 @@
-
-num = int(input("Enter a number: "))
-match num:
-    case  num if num<0:
-        print("Negative")
-    case num if num>0:
-        print("Positive")
-    case _:
-        print("Zero")
